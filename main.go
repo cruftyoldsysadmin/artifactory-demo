9aceab2b72594ad8d2f9001c8893fe52fed2a280
cruftyoldsysadmin <cruftyoldsysadmin@users.noreply.github.com>
2023-04-17 20:40:34 +0300

    minor change

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-// Hello world package for testing Artifactory deployments with Go projects.
+// Hello world package for testing Artifactory deployments with Go projects
 
 func main() {
 	println("Hello, world!")
